6b97b914bf8ca1f64a9b913b7e713306dd71d09e
שמואל אוריה אשכנזי <uriyaas@gmail.com>
2019-01-17 21:30:00 +0200

    מחלקה חדשה

---
+++ DAL/distance_calculation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace DAL
+{
+    public class distance_calculation
+    {
+        distance_calculation(string  trainee_Adress, string tester_Adress)
+        {
+
+            string origin = "pisga 45 st. jerusalem";//or "תקווה פתח 100 העם אחד "etc.
+            string destination = "gilgal 78 st. ramat-gan";//or "גן רמת 10 בוטינסקי'ז "etc.
+            string KEY = @"qJohA5KvfKXcnOlya1sL0tGCyDwArh2A";
+            string url = @"https://www.mapquestapi.com/directions/v2/route" +
+             @"?key=" + KEY +
+             @"&from=" + origin +
+             @"&to=" + destination +
+             @"&outFormat=xml" +
+             @"&ambiguities=ignore&routeType=fastest&doReverseGeocode=false" +
+             @"&enhancedNarrative=false&avoidTimedConditions=false";
+            //request from MapQuest service the distance between the 2 addresses
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sreader = new StreamReader(dataStream);
+            string responsereader = sreader.ReadToEnd();
+            response.Close();
+            //the response is given in an XML format
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(responsereader);
+            if (xmldoc.GetElementsByTagName("statusCode")[0].ChildNodes[0].InnerText == "0")
+            //we have the expected answer
+            {
+                //display the returned distance
+                XmlNodeList distance = xmldoc.GetElementsByTagName("distance");
+                double distInMiles = Convert.ToDouble(distance[0].ChildNodes[0].InnerText);
+                Console.WriteLine("Distance In KM: " + distInMiles * 1.609344);
+                //display the returned driving time
+                XmlNodeList formattedTime = xmldoc.GetElementsByTagName("formattedTime");
+                string fTime = formattedTime[0].ChildNodes[0].InnerText;
+                Console.WriteLine("Driving Time: " + fTime);
+            }
+            else if (xmldoc.GetElementsByTagName("statusCode")[0].ChildNodes[0].InnerText == "402")
+            //we have an answer that an error occurred, one of the addresses is not found
+            {
+                Console.WriteLine("an error occurred, one of the addresses is not found. try again.");
+            }
+            else //busy network or other error...
+            {
+                Console.WriteLine("We have'nt got an answer, maybe the net is busy...");
+            }
+
+        }
+
+    }
+}
